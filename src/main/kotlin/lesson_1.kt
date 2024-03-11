8abfd31df5d55320e5680c5d3814a9440a911024
salikov <a.salikov@cft.ru>
2024-03-11 13:06:51 +0700

    KS-1-1 Создал и проинициализировал переменные

---
+++ src/main/kotlin/lesson_1.kt
@@ -0,0 +1,11 @@
+package org.example
+
+fun main(){
+
+    var numberOfOrders: Int = 75
+
+    var successOrderText: String = "Спасибо за покупку!"
+
+    println("Количество заказов: $numberOfOrders")
+    println(successOrderText)
+}
