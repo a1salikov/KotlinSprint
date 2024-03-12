80d91e2d9ca54bd3edef6792248d87a67e115bd5
salikov <a.salikov@cft.ru>
2024-03-12 22:53:33 +0700

    KS-2-1 Добавил вызов метода подсчёта среднего арифметического

--- src/main/kotlin/Lesson_2/lesson_2-1.kt
+++ src/main/kotlin/Lesson_2/lesson_2-1.kt
@@ -1,6 +1,10 @@
 package org.example.Lesson_2
 
-fun main(){}
+fun main(){
+
+    getAverageScore()
+
+}
 
 fun getAverageScore(){
 
@@ -11,9 +15,6 @@ fun getAverageScore(){
 }
 
 const val FIRST_PUPIL_SCORE = 3f
-
 const val SECOND_PUPIL_SCORE = 4f
-
 const val THIRD_PUPIL_SCORE = 3f
-
 const val FORTH_PUPIL_SCORE = 5f
