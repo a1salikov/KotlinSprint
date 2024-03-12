9c9f1e7f366ac53d3ee2ba81874521ca91c81ab0
salikov <a.salikov@cft.ru>
2024-03-12 22:49:51 +0700

    KS-2-1 Добавил оценки в виде констант

--- src/main/kotlin/Lesson_2/lesson_2-1.kt
+++ src/main/kotlin/Lesson_2/lesson_2-1.kt
@@ -1,2 +1,11 @@
 package org.example.Lesson_2
 
+fun main(){}
+
+const val FIRST_PUPIL_SCORE = 3
+
+const val SECOND_PUPIL_SCORE = 4
+
+const val THIRD_PUPIL_SCORE = 3
+
+const val FORTH_PUPIL_SCORE = 5
