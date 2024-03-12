52fb2246a10aaded23257df26713fec23520585a
salikov <a.salikov@cft.ru>
2024-03-12 10:06:58 +0700

    KS-1-2 Создал переменную numberOfWorkers и переприсвоил её значение

---
+++ src/main/kotlin/lesson_2.kt
@@ -0,0 +1,20 @@
+package org.example
+
+fun main(){
+
+    var numberOfOrders: Int = 75
+
+    var successOrderText: String = "Спасибо за покупку!"
+
+    println("Количество заказов: $numberOfOrders")
+    println(successOrderText)
+
+    var numberOfWorkers: Int = 2000
+
+    //   println("Количество работников: $numberOfWorkers")
+
+    numberOfWorkers -= 1
+
+    println("Количество работников: $numberOfWorkers")
+
+}
