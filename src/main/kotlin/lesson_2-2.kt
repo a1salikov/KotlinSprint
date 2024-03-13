c40fe1e291da1fd8a78d6c51f9f9146a34e015cc
salikov <a.salikov@cft.ru>
2024-03-13 12:58:45 +0700

    KS-2-2 Добавил значения из ТЗ в виде переменных

---
+++ src/main/kotlin/lesson_2-2.kt
@@ -0,0 +1,10 @@
+package org.example
+
+fun main(){
+
+    var numberOfPermanentEmployees = 50
+    var numberOfTemporaryEmployees = 30
+    var salaryOfPermanentEmployee = 30000
+    var salaryOfTemporaryEmployee = 20000
+
+}
