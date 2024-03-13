09c926f79280aebb55fa1f599c520d63a0ac66e8
salikov <a.salikov@cft.ru>
2024-03-13 17:40:46 +0700

    KS-2-4 Добавил переменные, содержащие результат требуемых вычислений и вывод в консоль

--- src/main/kotlin/lesson_2-4.kt
+++ src/main/kotlin/lesson_2-4.kt
@@ -5,4 +5,10 @@ fun main (){
     val crystalOre: Int = 7
     val ironOre: Int = 11
 
+    val bonusCrystalOre: Int = (crystalOre * 1.2).toInt() - crystalOre
+    val bonusIronOre: Int = (ironOre * 1.2).toInt() - ironOre
+
+    println(bonusCrystalOre)
+    println(bonusIronOre)
+
 }
