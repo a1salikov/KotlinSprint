529277ba33a5b04820564c26bf3525792130be0a
salikov <a.salikov@cft.ru>
2024-03-13 18:30:39 +0700

    KS-2-5 Добавил переменную, содержащую результат требуемых вычислений и вывод в консоль

--- src/main/kotlin/lesson_2-5.kt
+++ src/main/kotlin/lesson_2-5.kt
@@ -1,7 +1,12 @@
+import kotlin.math.pow
+
 fun main(){
 
     var clientData = 70000
     var percentage = 16.7
     var timeYears = 20
+
+    var capitalization = clientData * ((1 + percentage / 100)).pow(timeYears)
+    println("%.${3}f".format(capitalization))
 
 }
