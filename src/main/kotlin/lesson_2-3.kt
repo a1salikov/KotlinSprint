209936d7cf83ffe165c0e7297c50633166769215
salikov <a.salikov@cft.ru>
2024-03-14 10:40:20 +0700

    KS-2-3 Добавил переменные, принимающие результат требуемых вычислений и вывод в консоль

--- src/main/kotlin/lesson_2-3.kt
+++ src/main/kotlin/lesson_2-3.kt
@@ -2,9 +2,14 @@ package org.example
 
 fun main(){
 
-    val departureTimeHour: Int = 9
-    val departureTimeMinute: Int = 39
-    val travelTimeMinutes: Int = 457
+    val departureTimeHour = 9
+    val departureTimeMinute = 39
+    val travelTimeMinutes = 457
+
+    val sumTime = departureTimeHour * 60 + departureTimeMinute + travelTimeMinutes
+    val arriveHour = sumTime / 60
+    val arriveMin = sumTime % 60
+    println("$arriveHour:$arriveMin")
 
 }
 
