2f5c3f6c604473cdf469e422c34e6ea0ca493604
salikov <a.salikov@cft.ru>
2024-03-14 10:54:04 +0700

    KS-2-2 Поменял тип для переменных, которые не содержат в себе расчётов

--- src/main/kotlin/lesson_2-2.kt
+++ src/main/kotlin/lesson_2-2.kt
@@ -2,10 +2,10 @@ package org.example
 
 fun main(){
 
-    var numberOfPermanentEmployees = 50
-    var numberOfTemporaryEmployees = 30
-    var salaryOfPermanentEmployee = 30000
-    var salaryOfTemporaryEmployee = 20000
+    val numberOfPermanentEmployees = 50
+    val numberOfTemporaryEmployees = 30
+    val salaryOfPermanentEmployee = 30000
+    val salaryOfTemporaryEmployee = 20000
 
     var totalSalaryOfPermanentEmployees: Int = numberOfPermanentEmployees * salaryOfPermanentEmployee
     var totalSalary: Int = (numberOfPermanentEmployees * salaryOfPermanentEmployee) + (numberOfTemporaryEmployees + salaryOfTemporaryEmployee)
