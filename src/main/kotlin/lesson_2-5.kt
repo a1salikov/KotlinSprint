05690cc0ea97f939bf553174b32237eedb2f71dd
salikov <a.salikov@cft.ru>
2024-03-14 11:15:16 +0700

    KS-2-5 Поменял тип неизменяемых переменных

--- src/main/kotlin/lesson_2-5.kt
+++ src/main/kotlin/lesson_2-5.kt
@@ -2,9 +2,9 @@ import kotlin.math.pow
 
 fun main(){
 
-    var clientData = 70000
-    var percentage = 16.7
-    var timeYears = 20
+    val clientData = 70000
+    val percentage = 16.7
+    val timeYears = 20
 
     var capitalization = clientData * ((1 + percentage / 100)).pow(timeYears)
     println("%.${3}f".format(capitalization))
