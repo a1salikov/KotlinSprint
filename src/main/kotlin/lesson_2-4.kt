63a355114d6831b412f920911310e4157d84ba2d
salikov <a.salikov@cft.ru>
2024-03-14 11:13:43 +0700

    KS-2-4 Добавил переменную с процентами баффа, а переменные из унёс в константы

--- src/main/kotlin/lesson_2-4.kt
+++ src/main/kotlin/lesson_2-4.kt
@@ -2,13 +2,14 @@ package org.example
 
 fun main (){
 
-    val crystalOre: Int = 7
-    val ironOre: Int = 11
-
-    val bonusCrystalOre: Int = (crystalOre * 1.2).toInt() - crystalOre
-    val bonusIronOre: Int = (ironOre * 1.2).toInt() - ironOre
+    val productivityBuffPercents = 20
+    val bonusCrystalOre: Int = (CRYSTAL_ORE * (1 + productivityBuffPercents / 100)).toInt() - CRYSTAL_ORE
+    val bonusIronOre: Int = (IRON_ORE * (1 + productivityBuffPercents / 100)).toInt() - IRON_ORE
 
     println(bonusCrystalOre)
     println(bonusIronOre)
 
 }
+
+const val CRYSTAL_ORE = 7
+const val IRON_ORE = 11
